2c1b817ea2b5fd1066601a33dd8c3144c1e03862
Ihnatsevych Volodymir <ukakyn@gmail.com>
2025-02-15 14:47:14 +0200

    add database schema for Hospital with tables for Examinations, Doctors, Departments, and Diseases

--- main.sql
+++ main.sql
@@ -1,1 +1,37 @@
---Test
+create database Hospital;
+go
+use Hospital;
+go
+create table Examinations
+(
+    id          INT IDENTITY (1,1) PRIMARY KEY,
+    name        varchar(255) not null,
+    day_of_week varchar(50)  not null,
+    start_time  time         not null,
+    end_time    time         not null
+);
+go
+create table Doctors
+(
+    id        INT IDENTITY (1,1) PRIMARY KEY,
+    name      varchar(255) not null,
+    specialty varchar(255) not null
+);
+go
+create table Departments
+(
+    id   INT IDENTITY (1,1) PRIMARY KEY,
+    name varchar(255) not null
+);
+go
+create table Diseases
+(
+    id          INT IDENTITY (1,1) PRIMARY KEY,
+    name        varchar(255) not null,
+    description nvarchar(max),
+);
+go
+
+use master
+drop database Hospital;
+go
